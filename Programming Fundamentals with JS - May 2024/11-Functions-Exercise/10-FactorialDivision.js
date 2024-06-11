f0d26068dbf1ca5bfcea254a6fe36ce606137f89
Hristo <hristocvetomirov704@gmail.com>
2024-06-12 00:20:35 +0300

    Added all tasks

---
+++ Programming Fundamentals with JS - May 2024/11-Functions-Exercise/10-FactorialDivision.js
@@ -0,0 +1,20 @@
+function factorialDivision(num1, num2) {
+    
+    function calculateFactorial(num) {
+        let factorial = 1;
+
+        for (let factor = 2; factor  <= num; factor++) {
+            factorial *= factor;
+        }
+
+        return factorial;
+    }
+    
+    let factorial1 = calculateFactorial(num1);
+    let factorial2 = calculateFactorial(num2);
+
+    let quotient = factorial1 / factorial2;
+    console.log(quotient.toFixed(2));
+}
+
+factorialDivision(5, 2)
