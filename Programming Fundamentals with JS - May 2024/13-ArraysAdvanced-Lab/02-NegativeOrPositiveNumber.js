6ce31b82c7c6cde768696931b893a28e98d708d9
Hristo <hristocvetomirov704@gmail.com>
2024-06-14 00:23:46 +0300

    Added all tasks

---
+++ Programming Fundamentals with JS - May 2024/13-ArraysAdvanced-Lab/02-NegativeOrPositiveNumber.js
@@ -0,0 +1,14 @@
+function negativeOrPositiveNumber(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        
+        if (array[i] < 0) {
+            result.unshift(array[i]);
+        } else {
+            result.push(array[i]);
+        }
+    }
+    console.log(result.join(`\n`));
+}
+
+negativeOrPositiveNumber([`7`, `-2`, `8`, `9`])
