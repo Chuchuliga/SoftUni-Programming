27039c01a7cf8b503aa8cb785525cdbb7dbd21f5
Hristo <hristocvetomirov704@gmail.com>
2024-06-17 17:18:28 +0300

    Added all tasks

--- Programming Fundamentals with JS - May 2024/13-ArraysAdvanced-Lab/06-SmallestTwoNumbers.js
+++ Programming Fundamentals with JS - May 2024/13-ArraysAdvanced-Lab/06-SmallestTwoNumbers.js
@@ -1,8 +1,7 @@
 function smallestTwoNumbers(array) {
-    let sorted = array.sort((a, b) => {
-        return a - b;
-    })
-    console.log(sorted[0], sorted[1]);
+    let sorted = array.sort((a, b) => a - b)
+    console.log(sorted[0], sorted[1]); // mine option
+    console.log(sorted.slice(0, 2).join(` `)); // softuni option
 }
 
 smallestTwoNumbers([30, 15, 50, 5]) // 5 15
