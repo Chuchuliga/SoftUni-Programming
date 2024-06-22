6b67234c54ba490f391e7c3d72936b2693985d15
Hristo <hristocvetomirov704@gmail.com>
2024-06-22 15:07:51 +0300

    Added all tasks

---
+++ Programming Fundamentals with JS - May 2024/ExamPreparation-2.js/01-ComputerStore.js
@@ -0,0 +1,37 @@
+function computerStore(array) {
+    let totalPrice = 0;
+    let taxes = 0;
+    let totalPriceWithTaxes = 0;
+    for (let i = 0; i < array.length; i++) {
+        let prices = array[i];
+
+        while (prices !== `regular` && prices !== `special`) {
+
+            if (prices > 0) {
+                totalPrice += Number(prices)
+                taxes += prices * 0.20;
+                break;
+            } else {
+                console.log(`Invalid price!`);
+                break;
+            }
+        }
+        totalPriceWithTaxes = totalPrice + taxes;
+        if (prices === `special`) {
+            totalPriceWithTaxes *= 0.90;
+        }
+    }
+    if (totalPrice === 0) {
+        console.log(`Invalid order!`);
+        return;
+    }
+
+    console.log(`Congratulations you've just bought a new computer!`)
+    console.log(`Price without taxes: ${totalPrice.toFixed(2)}$`);
+    console.log(`Taxes: ${taxes.toFixed(2)}$`);
+    console.log(`-----------`);
+    console.log(`Total price: ${totalPriceWithTaxes.toFixed(2)}$`);
+}
+
+computerStore(['1050', '200', '450', '2', '18.50', '16.86', 'special'])
+computerStore(['regular']) 
