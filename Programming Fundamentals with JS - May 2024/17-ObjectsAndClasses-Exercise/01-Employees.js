bbaa0c29f86b637bebb6705a4993b5370ea1bd15
Hristo <hristocvetomirov704@gmail.com>
2024-07-01 14:06:33 +0300

    Added all tasks

--- Programming Fundamentals with JS - May 2024/17-ObjectsAndClasses-Exercise/01-Employees.js
+++ Programming Fundamentals with JS - May 2024/17-ObjectsAndClasses-Exercise/01-Employees.js
@@ -1,5 +1,6 @@
 function employees(array) {
     let lenght = [];
+
     class Name {
         constructor(name) {
             this.name = name;
@@ -7,24 +8,19 @@ function employees(array) {
     }
 
     for (let i = 0; i < array.length; i++) {
-        let name = array[i];
+        let employeeName = array[i];
         let personalNum = 0;
-        lenght.push(new Name(name[0]));
+        lenght.push(new Name(employeeName[0]));
 
         for (let j = 0; j < array.length; j++) {
-            personalNum = name.length;
+            personalNum = employeeName.length;
         }
-        console.log(`Name: ${name} -- Personal Number: ${personalNum}`);
+        console.log(`Name: ${employeeName} -- Personal Number: ${personalNum}`);
     }
 }
 
-// employees(['Silas Butler', 'Adnaan Buckley', 'Juan Peterson', 'Brendan Villarreal'])
+employees(['Silas Butler', 'Adnaan Buckley', 'Juan Peterson', 'Brendan Villarreal'])
 // Name: Silas Butler -- Personal Number: 12
 // Name: Adnaan Buckley -- Personal Number: 14
 // Name: Juan Peterson -- Personal Number: 13
 // Name: Brendan Villarreal -- Personal Number: 18
-// employees(['Samuel Jackson', 'Will Smith', 'Bruce Willis', 'Tom Holland'])
-// Name: Samuel Jackson -- Personal Number: 14
-// Name: Will Smith -- Personal Number: 10
-// Name: Bruce Willis -- Personal Number: 12
-// Name: Tom Holland -- Personal Number: 11    
