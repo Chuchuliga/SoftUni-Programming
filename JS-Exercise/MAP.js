d38f21bd6ee23d00462386bb97c88f99686a8335
Hristo <hristocvetomirov704@gmail.com>
2024-07-29 23:08:20 +0300

    Added all tasks

--- JS-Exercise/MAP.js
+++ JS-Exercise/MAP.js
@@ -3,7 +3,9 @@ let map = new Map();
 map.set(`Name`, `Ivan`);
 map.set(`LastName`, `Ivanov`);
 map.set(`Age`, 18);
+map.set(`Age`, 23);
 
 console.log(map);
 
 // MAP -> helps with keeping the order of the insertion of the key-value pairs
+// SET -> helps with keeping unique value inside
