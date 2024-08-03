cf1a631e6d68e47b73aa4dce2f65c9c931f48219
Hristo <hristocvetomirov704@gmail.com>
2024-08-03 22:10:03 +0300

    Added all tasks

--- Programming Fundamentals with JS - May 2024/09-Arrays-MoreExercises/08-DiagonalAttack.js
+++ Programming Fundamentals with JS - May 2024/09-Arrays-MoreExercises/08-DiagonalAttack.js
@@ -23,5 +23,7 @@ function diagonalAttack(input) {
     console.log(matrix.map(row => row.join(" ")).join("\n"));
 }
 
-// diagonalAttack(['1 1 1', '1 1 1', '1 1 0'] )
-diagonalAttack(['5 3 12 3 1', '11 4 23 2 5', '101 12 3 21 10', '1 4 5 2 2', '5 22 33 11 1'])
+diagonalAttack(['1 1 1', '1 1 1', '1 1 0'] )
+// 1 1 1
+// 1 1 1
+// 1 1 0
