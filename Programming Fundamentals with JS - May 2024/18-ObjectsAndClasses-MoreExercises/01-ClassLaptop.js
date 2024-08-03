6a38f7263884d15d80eb742f0ffc2115eb99cfef
Hristo <hristocvetomirov704@gmail.com>
2024-08-03 19:16:39 +0300

    Added all tasks

---
+++ Programming Fundamentals with JS - May 2024/18-ObjectsAndClasses-MoreExercises/01-ClassLaptop.js
@@ -0,0 +1,46 @@
+function classLaptop() {
+
+    class Laptop {
+        constructor (info, quality) {
+            this.info = info;
+            this.isOn = false;
+            this.quality = quality;
+        }
+
+        get price() {
+            return 800 - (this.info.age * 2) + (this.quality * 0.5);
+        }
+
+        turnOn() {
+            this.isOn = true;
+            this.quality--;
+        }
+
+        turnOff() {
+            this.isOn = false;
+            this.quality--;
+        }
+
+        showInfo() {
+            return JSON.stringify(this.info);
+        }
+    }
+
+    let info = {producer: "Dell", age: 2, brand: "XPS"}
+    let laptop = new Laptop(info, 10)
+    laptop.turnOn()
+    console.log(laptop.showInfo())
+    laptop.turnOff()
+    console.log(laptop.quality)
+    laptop.turnOn()
+    console.log(laptop.isOn)
+    console.log(laptop.price)
+
+    // Output:
+    // {"producer":"Dell","age":2,"brand":"XPS"}
+    // 8
+    // true
+    // 799.5
+}
+
+classLaptop()
