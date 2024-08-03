925fc9c48dddfc3f86b904e35b303a3f9b15f9c9
Hristo <hristocvetomirov704@gmail.com>
2024-08-03 22:35:05 +0300

    Added all tasks

---
+++ Programming Fundamentals with JS - May 2024/26-RegularExpressions-Exercise/05-NetherRealms.js
@@ -0,0 +1,29 @@
+function netherRealms(participants) {
+    const lines = participants.split(/[\s,]+/);
+
+    let demons = {};
+
+    lines.forEach(line => {
+        const healthMatch = line.match(/[^0-9+-/*.]/g);
+        const health = healthMatch ? healthMatch.reduce((a, char) => a + char.charCodeAt(0), 0) : 0;
+        const dmg = (line.match(/[+-]?\d+(\.\d+)?/g) || []).reduce((a, number) => a + Number(number), 0);
+        const altering = line.match(/[*/]/g);
+
+        demons[line] = { health, dmg };
+
+        if (altering) {
+            altering.forEach(alter => {
+                demons[line].dmg *= alter === '*' ? 2 : 0.5;
+            });
+        }
+    });
+
+    const filteredByName = Object.entries(demons).sort((a, b) => a[0].localeCompare(b[0]));
+
+    for (const [name, obj] of filteredByName) {
+        console.log(`${name} - ${obj.health} health, ${obj.dmg.toFixed(2)} damage`);
+    }
+}
+
+netherRealms('M3ph-0.5s-0.5t0.0**')
+// M3ph-0.5s-0.5t0.0** - 524 health, 8.00 damage
---
+++ Programming Fundamentals with JS - May 2024/26-RegularExpressions-Exercise/06-ExtractEmails.js
@@ -0,0 +1,11 @@
+function extractEmails(text) {
+    const emailRegex = /(^|(?<=\s))([A-Za-z0-9])+([\.\-\_][A-Za-z0-9]+)*@([A-Za-z-]+)\.([A-Za-z-]+)([\.A-Za-z-])*(\b|(?=\s))/g;
+    const matches = text.match(emailRegex);
+
+    if (matches) {
+        matches.forEach(email => console.log(email.trim()));
+    }
+}
+
+extractEmails('Please contact us at: support@github.com.')
+//support@github.com
