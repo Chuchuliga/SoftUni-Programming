280d2a33f91b6d4cb71aa8b9ccef5e6f19c25fb8
Hristo <hristocvetomirov704@gmail.com>
2024-09-26 10:46:56 +0300

    k

---
+++ JS Advanced - September 2024/07-DOMIntroduction-Lab/sumNumbers/03-SumNumbers.js
@@ -0,0 +1,8 @@
+function sumNumbers() {
+    const a = document.getElementById('num1');
+    const b = document.getElementById('num2');
+    const output = document.getElementById('sum');
+
+    const sum = Number(a.value) + Number(b.value);
+    output.value = sum;
+}
