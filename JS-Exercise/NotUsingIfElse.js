5a25482e95ed9e5fd6a6c98c39a5ea62d7138bd2
Hristo <hristocvetomirov704@gmail.com>
2024-10-26 00:59:39 +0300

    Added all tasks

--- JS-Exercise/NotUsingIfElse.js
+++
@@ -1,8 +0,0 @@
-function lowerOrUpper(char) {
-    char === char.toUpperCase() ? console.log(`upper-case`) : console.log(`lower-case`);
-    //      /\                 /\              /\                            /\
-    //      ||                 ||              ||                            ||
-    // имаме въпрос   задължително ?    ако е верен изпечатай това     ако не е верен изпечатай това 
-}
-
-lowerOrUpper(`L`)
--- JS-Exercise/Pen4oPen4ev.js
+++
@@ -1,7 +0,0 @@
-function greetingByName(input) {
-    let firstName = input[0];
-    let lastName = input[1];
-    console.log(`Hello, ${firstName} ${lastName}!`);
-}
-
-greetingByName(["Pen4o", "Pen4ev"])
